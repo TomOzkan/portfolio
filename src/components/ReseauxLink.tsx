7a8a18a5d4464f97312913e762ad36325fc18512
Tom <tomozkan0@icloud.com>
2024-03-31 01:03:54 +0100

    first commit

---
+++ src/components/ReseauxLink.tsx
@@ -0,0 +1,10 @@
+export default function ReseauxLink() {
+  return (
+    <div className="z-10 flex gap-4">
+      <a href="https://www.linkedin.com/in/tom-ozkan-133693210/">Linkedin</a>
+      <a href="https://github.com/TomOzkan">Github</a>
+      <a href="https://gitlab.com/TomOzkan">Gitlab</a>
+      <a href="mailto:tomozkan0@icloud.com">Mail</a>
+    </div>
+  );
+}
